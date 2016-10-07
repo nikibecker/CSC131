6c61069ae543f876e10fc240b789f30b7b0d9419
Coilenara <brandon_1028@yahoo.com>
2016-10-06 23:07:51 -0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using StudentAttendance.Models;
+using System.Diagnostics;
+
+namespace StudentAttendance.Controllers
+{
+    public class HomeController : Controller
+    {
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Updated(UserInformation ui)
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            String spreadsheetId2 = "14-UKYNismFO3QP9H20eMbkvumnzwnApoC9vu2VnMjTQ";
+
+            String range = "D2:D40";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId2, range);
+
+            string range2;
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    if (ui.studentID == (string)row[0])
+                    {
+                        range2 = "F26";
+                        ValueRange valueRange = new ValueRange();
+                        valueRange.MajorDimension = "COLUMNS";          //"ROWS";//COLUMNS
+                        var oblist = new List<object>() { "Y" };
+                        valueRange.Values = new List<IList<object>> { oblist };
+                        SpreadsheetsResource.ValuesResource.UpdateRequest update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId2, range2);
+                        update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+                        UpdateValuesResponse result2 = update.Execute();
+                        return View();
+                    }
+                }
+            }
+            return View("Index");
+        }
+
+    }
+}
---
+++ Models/UserInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAttendance.Models
+{
+    public class UserInformation
+    {
+        public string studentID { get; set; }
+    }
+}
